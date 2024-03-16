5925f5a34fabe16547315d43bf062a9fddd0afe5
Willaume Cedric <cedric.willaume@ecole.ensicaen.fr>
2024-03-16 21:45:06 +0100

    demo.py

---
+++ Projet2A_front-end/Menu/Demo/demo.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, send_from_directory
+from flask_cors import CORS
+import os
+import subprocess  
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile as sf
+
+app = Flask(__name__, static_folder='spectrogrammes')
+CORS(app)
+
+def supprimer_fichier(chemin_fichier):
+    if os.path.exists(chemin_fichier):
+        os.remove(chemin_fichier)
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    fichier_audio = './uploads/audio_converted.wav'
+    fichier_spectrogramme = './spectrogrammes/spectrogram.png'
+    supprimer_fichier(fichier_audio)
+    supprimer_fichier(fichier_spectrogramme)
+    if 'audio' not in request.files:
+        return 'Aucun fichier audio trouvé', 400
+
+    audio = request.files['audio']
+    filename = 'audio_received.wav'
+    path_to_save = os.path.join('uploads', filename)
+    audio.save(path_to_save)
+
+    converted_filename = 'audio_converted.wav'
+    path_to_converted = os.path.join('uploads', converted_filename)
+    subprocess.run(['D:\\Application\\ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe', '-i', path_to_save, '-acodec', 'pcm_s16le', '-ar', '44100', path_to_converted], check=True)
+
+    y, sr = librosa.load(path_to_converted, sr=None)
+    sf.write(path_to_converted, y, sr, format='wav')
+
+    S = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(S, sr=sr, x_axis='time', y_axis='mel')
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
+    spectrogram_path = os.path.join('spectrogrammes', 'spectrogram.png')
+    plt.savefig(spectrogram_path)
+    plt.close()
+
+    full_spectrogram_path = os.path.abspath(spectrogram_path)
+    return {'spectrogramPath': full_spectrogram_path}
+
+@app.route('/<path:path>')
+def static_file(path):
+    return send_from_directory('spectrogrammes', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
