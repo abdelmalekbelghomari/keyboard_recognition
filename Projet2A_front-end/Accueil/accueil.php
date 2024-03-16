29ea99aed0a7f6cf535afd4404f41e6cda5a8c70
Willaume Cedric <cedric.willaume@ecole.ensicaen.fr>
2024-03-16 21:39:00 +0100

    accueil.php

---
+++ Projet2A_front-end/Accueil/accueil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil.css">
+    <script src="accueil.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    
+    <title>projet</title>
+</head>
+<body>
+    <div class="hacker-background">
+        <div class="content">
+            <h1 id="typing-text"></h1>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
