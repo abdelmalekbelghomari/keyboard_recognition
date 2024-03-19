fddd17a210f24bf8a4045346682f6693dbd834d5
sriha <haykel.sriha@ecole.ensicaen.fr>
2024-03-19 13:01:07 +0100

    added a save option for the scaler and the model

--- Orange/script.py
+++ Orange/script.py
@@ -2,6 +2,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from joblib import load, dump
 
 df = pd.read_csv('output2.csv')
 
@@ -21,4 +22,22 @@ model.fit(X_train_scaled, y_train)
 print("Score de précision sur l'ensemble d'entraînement:", model.score(X_train_scaled, y_train))
 print("Score de précision sur l'ensemble de test:", model.score(X_test_scaled, y_test))
 
+# Prédictions du modèle sur l'ensemble de test
+predictions = model.predict(X_test_scaled)
 
+# Affichage des prédictions et des valeurs réelles
+print("\nPrédictions du modèle sur l'ensemble de test:")
+print(predictions)
+
+print("\nValeurs réelles de l'ensemble de test:")
+print(y_test)
+
+# Enregistrement du modèle
+model_filename = '../models/mlp_model.joblib'
+dump(model, model_filename)
+print(f"Modèle enregistré sous le nom : {model_filename}")
+
+# Enregistrement du scaler
+scaler_filename = '../scalers/scaler.joblib'
+dump(scaler, scaler_filename)
+print(f"Scaler enregistré sous le nom : {scaler_filename}")
